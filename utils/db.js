f9aa3ee1073c5378ac95b0059cb5c96031f93b91
settpaingt67-creator <settpaing.t67@rsu.ac.th>
2025-11-28 17:51:06 +0700

    db.js

--- utils/db.js
+++ utils/db.js
@@ -1,6 +1,6 @@
 const mysql = require('mysql2');
 
-const mysqlPool = mysql.createPool({
+export const mysqlPool = mysql.createPool({
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASS || '',
@@ -8,7 +8,8 @@ const mysqlPool = mysql.createPool({
   port: Number(process.env.DB_PORT || 3306),
   waitForConnections: true,
   connectionLimit: 10,
+  ssl: process.env.DB_SSL === 'true' ? { minVersion: 'TLSv1.2', rejectUnauthorized: true } : undefined,
 });
 
-module.exports = { mysqlPool };
+
 
